d3bd9ddc694e9e9f6d22ead77df0a96bbafa66c2
Josh Pollock <josh@calderawp.com>
2018-06-23 11:52:25 -0400

    updates to components 0.4

--- src/module.js
+++ src/module.js
@@ -1,25 +1,10 @@
 /*eslint no-undef: "error"*/
 /*eslint-env node*/
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
 import CalderaProcessors from './CalderaProcessors';
 import CalderaProcessorsWithState from './CalderaProcessorsWithState';
-import { Provider } from 'react-redux';
 import {processorsStore} from './state/processorsStore';
-
-
 export default  {
 	CalderaProcessors,
 	CalderaProcessorsWithState,
-	processorsStore,
-	factory: function(element, store = processorsStore )  {
-		return ReactDOM.render(
-			<Provider store={store}>
-				<CalderaProcessorsWithState />
-			</Provider>,
-			element
-		);
-
-	}
+	processorsStore
 }
